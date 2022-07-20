4a1d79de0c7e3fbca8d8305d331afbfddc686b7d
Tarsis <tarsis.souza@ifal.edu.br>
2022-07-20 17:25:40 -0300

    Aula 12 - ListView builder - 20/07
    Nesta aula foram abordados o elemento ListView.builder.builder por meio do  desenvolvimento de telas semelhantes ao aplicativo Hotel

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:aula_924/data/BD.dart';
 import 'package:aula_924/domain/pacote_turistico.dart';
 import 'package:aula_924/widgets/card_pacote_turistico.dart';
 import 'package:flutter/material.dart';
@@ -10,45 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  PacoteTuristico pacote1 = PacoteTuristico(
-    imagem: 'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
-    titulo: 'Pacote Maragogi 2023',
-    transporte: 'Hotel All inclusive',
-    cidade: 'MARAGOGI, BRA',
-    validade: 'De 01 ago 2022 a 31 dez 2023',
-    desconto: 45,
-    numDiarias: 7,
-    numPessoas: 3,
-    numParcelas: 12,
-    precoAntigo: 4819,
-    precoAtual: 819,
-  );
-  PacoteTuristico pacote2 = PacoteTuristico(
-    imagem: 'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-    titulo: 'Pacote Cancún 2022/2023',
-    transporte: 'Aéreo - Hotel All inclusive',
-    validade: 'De 01 ago 2022 a 31 dez 2023',
-    cidade: 'CANCÚN, MEX',
-    desconto: 45,
-    numDiarias: 5,
-    numPessoas: 2,
-    numParcelas: 6,
-    precoAntigo: 6819,
-    precoAtual: 2819,
-  );
-  PacoteTuristico pacote3 = PacoteTuristico(
-    imagem: 'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-    titulo: 'Pacote Carneiros 2022/2025',
-    transporte: 'Aéreo',
-    validade: 'De 01 ago 2022 a 31 dez 2023',
-    cidade: 'RECIFE, BRA',
-    desconto: 45,
-    numDiarias: 3,
-    numPessoas: 1,
-    numParcelas: 6,
-    precoAntigo: 2000,
-    precoAtual: 1200,
-  );
+  List<PacoteTuristico> list = BD.lista;
 
   @override
   Widget build(BuildContext context) {
@@ -63,69 +26,79 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.grey[100],
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.purple,
-                borderRadius: BorderRadius.circular(16),
-              ),
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const Text(
-                          'Tops destinos mais buscados',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const Text(
-                          'Corre que tá rolando muita promoção',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'EU QUERO!',
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const Text(
+                            'Tops destinos mais buscados',
                             style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.black,
+                              fontSize: 24,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Color(0xFFF8FF04),
+                          const Text(
+                            'Corre que tá rolando muita promoção',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                            ),
                           ),
-                        )
-                      ],
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'EU QUERO!',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Color(0xFFF8FF04),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 24),
-                  const Placeholder(
-                    fallbackHeight: 150,
-                    fallbackWidth: 100,
-                    color: Colors.white,
-                  ),
-                ],
+                    const SizedBox(width: 24),
+                    const Placeholder(
+                      fallbackHeight: 150,
+                      fallbackWidth: 100,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            CardPacoteTuristico(pacoteTuristico: pacote1),
-            CardPacoteTuristico(pacoteTuristico: pacote2),
-            CardPacoteTuristico(pacoteTuristico: pacote3),
-          ],
-        ),
-      ),
+              const SizedBox(height: 16),
+              buildListView()
+            ],
+          )),
+    );
+  }
+
+  buildListView() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        return CardPacoteTuristico(
+          pacoteTuristico: list[index],
+        );
+      },
     );
   }
 }
